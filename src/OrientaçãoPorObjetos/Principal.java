af6c42652053e406440fb5c7e1e60fa56ce0c9a1
ze <jvitinhosa@gmail.com>
2025-08-28 11:44:15 -0300

    Implementa conceitos de Orientação a Objetos: atributos, instância, métodos e encapsulamento

--- src/OrientaçãoPorObjetos/Principal.java
+++ src/OrientaçãoPorObjetos/Principal.java
@@ -5,21 +5,16 @@ import OrientaçãoPorObjetos.Screenmatchmodels.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
+
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
-        //CTRL + CLIQUE = Navegação entre abas
 
-        meuFilme.exibeFichaTecnica();
+        meuFilme.exibirFicha();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Total de avaliações " + meuFilme.getTotalDeAvaliacao());
-        System.out.println(meuFilme.pegaMedia());
-        /*
-        meuFilme.somaDasAvaliacoes = 10;
-        meuFilme.totalDeAvaliacao = 1;
-        System.out.println(meuFilme.pegaMedia());//encapsulamento
-        */
+        System.out.println("Total de avalições: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegarMedia());
     }
 }
--- src/OrientaçãoPorObjetos/Screenmatchmodels/Filme.java
+++ src/OrientaçãoPorObjetos/Screenmatchmodels/Filme.java
@@ -1,15 +1,14 @@
 package OrientaçãoPorObjetos.Screenmatchmodels;
 
 public class Filme {
-    public String nome;
+    private String nome;
     private int anoDeLancamento;
-    private boolean incluindoNoPlano;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
-    private int totalDeAvaliacao;
-    private int duracaoEmMinutos;
+    private int totalDeAvaliacoes;
+    private int  duracaoEmMinutos;
 
 
-    //GETTERS
     public String getNome() {
         return nome;
     }
@@ -18,53 +17,45 @@ public class Filme {
         return anoDeLancamento;
     }
 
-    public boolean isIncluindoNoPlano() {
-        return incluindoNoPlano;
+    public boolean getIncluidoNoPlano() {
+        return incluidoNoPlano;
     }
 
     public int getDuracaoEmMinutos() {
         return duracaoEmMinutos;
     }
 
-    //get -> Obter valor
-    public int getTotalDeAvaliacao() {
-        return totalDeAvaliacao;
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
     }
-    /*****************************************************************/
 
-    //Setters
-
-    //ALT + INSERT = setter(Resultado aqui em baixo)
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public void setIncluindoNoPlano(boolean incluindoNoPlano) {
-        this.incluindoNoPlano = incluindoNoPlano;
     }
 
     public void setAnoDeLancamento(int anoDeLancamento) {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
     public void setDuracaoEmMinutos(int duracaoEmMinutos) {
         this.duracaoEmMinutos = duracaoEmMinutos;
     }
-    /**************************************************************/
 
-
-    public void exibeFichaTecnica() {
+    public void exibirFicha() {
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
-        //String       +        //variavel = concatenação
     }
 
     public void avalia(double nota) {
         somaDasAvaliacoes += nota;
-        totalDeAvaliacao++;
+        totalDeAvaliacoes++;
     }
 
-    public double pegaMedia() {
-        return somaDasAvaliacoes / totalDeAvaliacao;
+    public double pegarMedia() {
+        return somaDasAvaliacoes/totalDeAvaliacoes;
     }
 }
